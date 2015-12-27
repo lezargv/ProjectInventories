53334abb3d57f2891c149cdc8587fd3106fe884f
TrenTech <TrenTech@gmail.com>
2015-12-27 13:55:37 -0500

    Minor changes

--- src/com/gmail/trentech/pji/commands/CMDHelp.java
+++ src/com/gmail/trentech/pji/commands/CMDHelp.java
@@ -29,19 +29,23 @@ public class CMDHelp implements CommandExecutor {
 		}
 		String command = args.<String>getOne("command").get().toUpperCase();
 		String description = null;
+		String syntax = null;
 		String example = null;
 		
 		switch(command.toLowerCase()){
 			case "create":
 				description = " Allows you to create new inventory table.";
+				syntax = " /inv create <inventory>";
 				example = " /inv create NewInventory";
 				break;
 			case "delete":
 				description = " Allows you to delete existing inventory table.";
+				syntax = " /inv delete <inventory>";
 				example = " /inv delete OldInventory";
 				break;
 			case "set":
 				description = " Allows you to set a an inventory for a world.";
+				syntax = " /inv set <world> <inventory>";
 				example = " /inv set world NewInventory";
 				break;
 			default:
@@ -49,11 +53,11 @@ public class CMDHelp implements CommandExecutor {
 				return CommandResult.empty();
 		}
 		
-		help(command, description, example).sendTo(src);
+		help(command, description, syntax, example).sendTo(src);
 		return CommandResult.success();
 	}
 	
-	private PaginationBuilder help(String command, String description, String example){
+	private PaginationBuilder help(String command, String description, String syntax, String example){
 		PaginationBuilder pages = Main.getGame().getServiceManager().provide(PaginationService.class).get().builder();
 		pages.title(Texts.builder().color(TextColors.DARK_GREEN).append(Texts.of(TextColors.AQUA, command)).build());
 		
@@ -61,8 +65,10 @@ public class CMDHelp implements CommandExecutor {
 
 		list.add(Texts.of(TextColors.AQUA, "Description:"));
 		list.add(Texts.of(TextColors.GREEN, description));
+		list.add(Texts.of(TextColors.AQUA, "Syntax:"));
+		list.add(Texts.of(TextColors.GREEN, syntax));
 		list.add(Texts.of(TextColors.AQUA, "Example:"));
-		list.add(Texts.of(TextColors.GREEN,  example, TextColors.DARK_PURPLE));
+		list.add(Texts.of(TextColors.GREEN,  example, TextColors.DARK_GREEN));
 		
 		pages.contents(list);
 		
