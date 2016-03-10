18a0625523a7a8e29ae352082f29684bd95b5ae3
TrenTech@gmail.com <TrenTech@gmail.com>
2016-03-10 00:10:24 -0500

    bump version

--- src/com/gmail/trentech/pji/utils/Resource.java
+++ src/com/gmail/trentech/pji/utils/Resource.java
@@ -3,7 +3,7 @@ package com.gmail.trentech.pji.utils;
 public class Resource {
 
 	public final static String NAME = "Project Inventories";
-	public final static String VERSION = "0.5.4";
+	public final static String VERSION = "0.5.5";
 	public final static String ID = "com.gmail.trentech.pji";
 	public final static String AUTHOR = "TrenTech";
 	public final static String URL = "http://github.com/TrenTech/ProjectInventories";
