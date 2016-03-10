ebefcb80bfe553193f02c840d70e3ac17db18781
TrenTech@gmail.com <TrenTech@gmail.com>
2016-03-09 23:04:08 -0500

    Fix gradle, update @Plugin to generate mcmod.info

--- src/com/gmail/trentech/pji/utils/Resource.java
+++ src/com/gmail/trentech/pji/utils/Resource.java
@@ -4,6 +4,9 @@ public class Resource {
 
 	public final static String NAME = "Project Inventories";
 	public final static String VERSION = "0.5.4";
-	public final static String ID = "pji";
+	public final static String ID = "com.gmail.trentech.pji";
+	public final static String AUTHOR = "TrenTech";
+	public final static String URL = "http://github.com/TrenTech/ProjectInventories";
+	public final static String DESCRIPTION = "Multi world inventories";
 
 }
