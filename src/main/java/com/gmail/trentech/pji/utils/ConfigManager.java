2c0011bc84eb6c6d2bca89811f79070f3bae8b93
TrenTech@gmail.com <TrenTech@gmail.com>
2016-07-28 22:35:39 -0400

    Fix naming convention

--- src/main/java/com/gmail/trentech/pji/utils/ConfigManager.java
+++ src/main/java/com/gmail/trentech/pji/utils/ConfigManager.java
@@ -16,7 +16,7 @@ public class ConfigManager {
 	private ConfigurationLoader<CommentedConfigurationNode> loader;
 
 	public ConfigManager(String folder, String configName) {
-		folder = "config" + File.separator + "projectinventories" + File.separator + "inventories" + File.separator + folder;
+		folder = "config" + File.separator + Resource.NAME.toLowerCase() + File.separator + "inventories" + File.separator + folder;
 		if (!new File(folder).isDirectory()) {
 			new File(folder).mkdirs();
 		}
@@ -27,7 +27,7 @@ public class ConfigManager {
 	}
 
 	public ConfigManager(String configName) {
-		String folder = "config" + File.separator + "projectinventories";
+		String folder = "config" + File.separator + Resource.NAME.toLowerCase();
 		if (!new File(folder).isDirectory()) {
 			new File(folder).mkdirs();
 		}
@@ -38,7 +38,7 @@ public class ConfigManager {
 	}
 
 	public ConfigManager() {
-		String folder = "config" + File.separator + "projectinventories";
+		String folder = "config" + File.separator + Resource.NAME.toLowerCase();
 		if (!new File(folder).isDirectory()) {
 			new File(folder).mkdirs();
 		}
--- src/main/java/com/gmail/trentech/pji/utils/Resource.java
+++ src/main/java/com/gmail/trentech/pji/utils/Resource.java
@@ -2,10 +2,11 @@ package com.gmail.trentech.pji.utils;
 
 public class Resource {
 
-	public final static String NAME = "Project Inventories";
+	public final static String NAME = "@project.name@";
+	public final static String ID = "@project.id@";
 	public final static String VERSION = "@project.version@";
-	public final static String ID = "com.gmail.trentech.pji";
-	public final static String AUTHOR = "TrenTech";
-	public final static String URL = "http://github.com/TrenTech/ProjectInventories";
+	public final static String DESCRIPTION = "@project.description@";
+	public final static String AUTHOR = "@project.author@";
+	public final static String URL = "@project.url@";
 
 }
