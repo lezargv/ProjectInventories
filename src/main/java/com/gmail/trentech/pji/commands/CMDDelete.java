627f76b6318374febbf94b52f5c7868ae3c1dda4
TrenTech <TrenTech@gmail.com>
2017-06-05 20:15:52 -0400

    Fix message

--- src/main/java/com/gmail/trentech/pji/commands/CMDDelete.java
+++ src/main/java/com/gmail/trentech/pji/commands/CMDDelete.java
@@ -24,7 +24,7 @@ public class CMDDelete implements CommandExecutor {
 		InventoryData inventoryData = args.<InventoryData>getOne("inv").get();
 
 		if (inventoryData.getName().equalsIgnoreCase("DEFAULT")) {
-			throw new CommandException(Text.of(TextColors.RED, inventoryData.getName(), " inventory be altered or deleted"), false);
+			throw new CommandException(Text.of(TextColors.RED, inventoryData.getName(), " inventory cannot be altered or deleted"), false);
 		}
 
 		src.sendMessage(Text.builder().color(TextColors.RED).append(Text.of(TextColors.RED, "[WARNING] ", TextColors.YELLOW, "This will delete players inventories and cannot be undone. Confirm? ")).onClick(TextActions.runCommand("/pji:inventory delete yes")).append(Text.of(TextColors.DARK_PURPLE, TextStyles.UNDERLINE, "/inventory delete yes")).build());
